c72e51fdc36b3bd494fbbbba9e93c377c4407d11
gegorov <gegorov@yahoo.com>
2017-04-08 14:23:45 +0300

    impementation via ArrayList

--- 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -3,7 +3,9 @@ package com.javarush.task.task08.task0829;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /* 
@@ -15,30 +17,27 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //list of addresses
-        HashMap<String,String> addresses = new HashMap<>();
+        List<String> addresses = new ArrayList<>();
         while (true) {
             String family = reader.readLine();
-
             if (family.isEmpty()) {
                 break;
             } else {
-                String city = reader.readLine();
-                addresses.put(city,family);
+                addresses.add(family);
+                //нечетные будут города
+                addresses.add(reader.readLine());
             }
         }
 
-        //read city
+        //read home number
         String city = reader.readLine();
-        // search in the array
 
-        for (Map.Entry<String, String> entry: addresses.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-
-            if (city.equals(key)) {
-                System.out.println(value);
+        for (String entry: addresses) {
+            if (city.equals(entry)) {
+                System.out.println(addresses.get(addresses.indexOf(entry) - 1));
             }
-            
+
         }
+
     }
 }
